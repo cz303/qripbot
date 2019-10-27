86638a14ca51358a19d3084dba68fce67f434956
cz303 <56400025+cz303@users.noreply.github.com>
2019-10-28 00:35:44 +0300

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -12,9 +12,9 @@ Follow Us on <a href='http://twitter.com/QRIPPLEX_PPL'>Twitter</a> (1000 PPL)"""
 
 ADMIN_ID = 677805757
 
-EMAIL_REGEX = r"?(\w){1,15}$"
-ERC20_REGEX = r"?(\w){1,15}$"
-USERNAME_REGEX = r"^@?(\w){1,15}$"
+EMAIL_REGEX = r"^@?(\w){1,100}$"
+ERC20_REGEX = r"^@?(\w){1,100}$"
+USERNAME_REGEX = r"^@^@?(\w){1,15}$"
 
 REGISTRATION_BONUS = 0
 REFERER_BONUS = 5
