1d347527731a61a7b5111cbcf8dd4c96e18f2eb9
cz303 <56400025+cz303@users.noreply.github.com>
2019-10-28 03:03:26 +0300

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -14,26 +14,26 @@ https://dstarlab.com"""
 
 ADMIN_ID = 677805757
 
-EMAIL_REGEX = r"^@?(\w){1,40}$"
-ERC20_REGEX = r"^@?(\w){1,40}$"
+DSTAR_REGEX = r"^@?(\w){1,40}$"
+TRX_REGEX = r"^@?(\w){1,40}$"
 USERNAME_REGEX = r"^@?(\w){1,15}$"
 
 REGISTRATION_BONUS = 0
 REFERER_BONUS = 5
 
 WRONG_CAPTCHA_MESSAGE = "Wrong captcha."
-WRONG_EMAIL_MESSAGE = "Wrong dSTAR format."
+WRONG_DSTAR_MESSAGE = "Wrong dSTAR format."
 WRONG_USERNAME_MESSAGE = "Wrong twitter username format."
-WRONG_ERC20_MESSAGE = "Wrong TRX format."
+WRONG_TRX_MESSAGE = "Wrong TRX format."
 
-CONFIRM_EMAIL_MESSAGE = "Do you confirm %s as your dSTAR username?"
+CONFIRM_DSTAR_MESSAGE = "Do you confirm %s as your dSTAR messenger username?"
 CONFIRM_USERNAME_MESSAGE = "Do you confirm %s as your twitter username?"
-CONFIRM_ERC20_MESSAGE = "Do you confirm %s as your TRX address?"
+CONFIRM_TRX_MESSAGE = "Do you confirm %s as your TRX address?"
 
-ASK_EMAIL_MESSAGE = "Send me your dSTAR username:"
+ASK_DSTAR_MESSAGE = "Send me your dSTAR username:"
 ASK_CAPTCHA_MESSAGE = "Enter the characters you see in the image"
 ASK_USERNAME_MESSAGE = "Nice, now send me your Twitter username"
-ASK_ERC20_MESSAGE = "Just one more step! Send me your TRX wallet address"
+ASK_TRX_MESSAGE = "Just one more step! Send me your TRX wallet address"
 
 REGISTRATION_SUCCESS_MESSAGE = "Congratulations! You're registered!\nIncrease your balance by sharing this link: %s"
 REGISTRATION_ABORTED_MESSAGE = "Registration aborted."
